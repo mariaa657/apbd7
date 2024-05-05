2032c634413cd3f0eabe8b7deac5af933933f266
Maria Leonczuk <maria76a@gmail.com>
2024-05-05 19:30:24 +0200

    commit 1

---
+++ cw6/Models/Wherehouse.cs
@@ -0,0 +1,8 @@
+﻿namespace cw6.Models;
+
+public class Wherehouse
+{
+    public int IdWherehouse { get; set; }
+    public string Name { get; set; }
+    public string Adress { get; set; }
+}
---
+++ cw6/Services/DbServices.cs
@@ -0,0 +1,14 @@
+﻿namespace cw6.Services;
+
+public interface IDbService
+{
+    Task<Student?> GetStudentDetailsById(int id);
+    Task<Group?> GetGroupById(int id);
+    Task<Student> AddStudentWithGroupsAssignments(Student student);
+}
+
+
+public class DbServices
+{
+    
+}
